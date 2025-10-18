761e21109e4f483a5cb8db9209551ed2b3b8e980
Félix <leuferbefils@gmail.com>
2025-10-18 18:42:50 +0100

    Modif de settings pour le test

--- Core/settings.py
+++ Core/settings.py
@@ -14,7 +14,7 @@ with open(BASE_DIR / '.secret_key', 'r') as f:
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1']
 
 
 # Application definition
